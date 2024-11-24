37de1e09583b2a57b481b3cb56efd94afc95590e
Ric Cox <ric.cox@gmail.com>
2024-11-24 10:19:10 -0700

    moved

--- pkg/job/protoc.sh
+++ pkg/job/protoc.sh
@@ -6,4 +6,4 @@
 # $ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 #
 
-protoc --go_out job --go_opt paths=source_relative --go-grpc_out job --go-grpc_opt paths=source_relative job.proto
+protoc --go_out . --go_opt paths=source_relative --go-grpc_out . --go-grpc_opt paths=source_relative job.proto
